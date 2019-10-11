4297317b41975bdda12e1821826c2c2a26d9aaa7
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-10-11 22:32:22 +0200

    Use the Arrange Act Assert pattern

--- tests/Tests.java
+++ tests/Tests.java
@@ -1,15 +1,24 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Tests {
     @Test
     void ATest() {
-        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
-        String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+        final List<MovieRental> rentals = Arrays.asList(
+                new MovieRental("F001", 3),
+                new MovieRental("F002", 1));
 
+        final String result = new RentalInfo().statement(
+                new Customer("C. U. Stomer", rentals));
+
+        final String expected = "Rental Record for C. U. Stomer\n" +
+                "\tYou've Got Mail\t3.5\n" +
+                "\tMatrix\t2.0\nAmount owed is 5.5\n" +
+                "You earned 2 frequent points\n";
         assertEquals(expected, result);
     }
 }
