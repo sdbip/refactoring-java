66c4525545a7ab94ab27f4b93988688ed6857424
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-10-12 10:15:47 +0200

    Add constants to explain the magic numbers

--- src/rentals/MovieType.java
+++ src/rentals/MovieType.java
@@ -2,15 +2,19 @@ package rentals;
 
 public enum MovieType {
     regular {
+        private static final int STANDARD_DURATION_DAYS = 2;
+        private static final double OVERDRAFT_FEE_PAER_DAY = 1.5;
+        private static final double BASE_PRICE = 2.0;
+
         @Override
         double getPrice(int daysRented) {
-            double thisAmount = 2;
-            if (daysRented > 2) {
-                thisAmount = ((daysRented - 2) * 1.5) + thisAmount;
-            }
-            return thisAmount;
+            return daysRented <= STANDARD_DURATION_DAYS ? BASE_PRICE :
+                    BASE_PRICE + ((daysRented - STANDARD_DURATION_DAYS) * OVERDRAFT_FEE_PAER_DAY);
         }
-    }, forChildren, newRelease;
+    },
+    forChildren,
+    newRelease;
+
 
     double getPrice(int daysRented) {
         return 0.0;
