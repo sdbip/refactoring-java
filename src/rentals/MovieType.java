72988c9eac1964f377ea25d16da4dee56f4ff8b0
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-10-12 10:19:37 +0200

    Fix a typo

--- src/rentals/MovieType.java
+++ src/rentals/MovieType.java
@@ -3,13 +3,13 @@ package rentals;
 public enum MovieType {
     regular {
         private static final int STANDARD_DURATION_DAYS = 2;
-        private static final double OVERDRAFT_FEE_PAER_DAY = 1.5;
+        private static final double OVERDRAFT_FEE_PER_DAY = 1.5;
         private static final double BASE_PRICE = 2.0;
 
         @Override
         double getPrice(int daysRented) {
             return daysRented <= STANDARD_DURATION_DAYS ? BASE_PRICE :
-                    BASE_PRICE + ((daysRented - STANDARD_DURATION_DAYS) * OVERDRAFT_FEE_PAER_DAY);
+                    BASE_PRICE + ((daysRented - STANDARD_DURATION_DAYS) * OVERDRAFT_FEE_PER_DAY);
         }
     },
     forChildren,
