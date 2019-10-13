36b894d7fbd1752ac4d88c8766137f3ac43904a3
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-10-13 22:32:27 +0200

    Change indentations

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,15 @@ import details.RentalInfo;
 import java.util.Arrays;
 
 class Main {
+    public static void main(String[] args) {
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
-  public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+        String result = new RentalInfo(new HardCodedMovieRepository()).statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
 
-    String result = new RentalInfo(new HardCodedMovieRepository()).statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+        if (!result.equals(expected)) {
+            throw new AssertionError("Expected: " + System.lineSeparator() + expected + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+        }
 
-    if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + expected + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+        System.out.println("Success");
     }
-
-    System.out.println("Success");
-  }
 }
