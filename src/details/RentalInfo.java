ad91e5667b9102029f5f27c52d272bf40cbd2a4f
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-10-13 23:12:16 +0200

    Convert rentals as a first step

--- src/details/RentalInfo.java
+++ src/details/RentalInfo.java
@@ -3,11 +3,12 @@ package details;
 import invoicing.Customer;
 import invoicing.LineItem;
 import invoicing.RentalStatement;
-import rentals.*;
 import rentals.MovieRental;
+import rentals.MovieRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RentalInfo {
@@ -18,24 +19,26 @@ public class RentalInfo {
         this.movieRepository = movieRepository;
     }
 
-    public String statement(Customer customer, List<details.MovieRental> rentals) {
-        final Stream<LineItem> lineItems =
-                streamRentals(rentals).map(r -> new LineItem(
+    public String statement(Customer customer, List<details.MovieRental> inputRentals) {
+        final Collection<MovieRental> rentals = convertRentals(inputRentals);
+        final Stream<LineItem> lineItems = rentals.stream()
+                .map(r -> new LineItem(
                         r.getMovieTitle(),
                         r.getPrice())
                 );
-        final double totalAmount = streamRentals(rentals)
+        final double totalAmount = rentals.stream()
                 .map(MovieRental::getPrice)
                 .reduce(0.0, Double::sum);
-        final int frequentRenterPoints = streamRentals(rentals)
+        final int frequentRenterPoints = rentals.stream()
                 .map(MovieRental::getFrequentRenterPoints)
                 .reduce(0, Integer::sum);
         final RentalStatement rentalStatement = new RentalStatement(customer, totalAmount, frequentRenterPoints, lineItems::iterator);
         return rentalStatement.toString();
     }
 
-    private Stream<MovieRental> streamRentals(Collection<details.MovieRental> rentals) {
+    private Collection<MovieRental> convertRentals(Collection<details.MovieRental> rentals) {
         return rentals.stream()
-                .map(r -> new MovieRental(movieRepository.get(r.movieId), r.days));
+                .map(r -> new MovieRental(movieRepository.get(r.movieId), r.days))
+                .collect(Collectors.toUnmodifiableList());
     }
 }
